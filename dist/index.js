d0d9be4a5eec91ea26cc1dbe7a42eb2f52a35b3e
Akash-tiwari05 <myselfakashtiwari05@gmail.com>
2026-09-25 17:51:29 +0530

    feat: my first ts code (Hello world)

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Hello ${person}! Welcome to code word`;
+}
+const username = "Typescript";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+function greet(person: string): string{
+
+    return `Hello ${person}! Welcome to code word`
+}
+
+const username: string = "Typescript"
+
+console.log(greet(username));
